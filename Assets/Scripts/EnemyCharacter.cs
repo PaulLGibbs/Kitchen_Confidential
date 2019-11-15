6fccfb45340b3ed04fd8e0e3638f3ad7ea54cf98
Doohl <dhlserrano@gmail.com>
2019-11-14 19:34:23 -0800

    Adds in the Character abstract class and refactors the player to fit into it as a child; implements the basic Enemy type; implements basic Player/Enemy attacking mechanics

---
+++ Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : Character
+{
+
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        hp = maxHP;
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    public override void OnDealDamage(Character target, int damage)
+    {
+        Debug.Log("Enemy trying to deal damage");
+    }
+
+    public override void OnDeath(Character source, int damage)
+    {
+        Destroy(gameObject);
+    }
+
+    public override void OnReceiveDamage(Character source, int damage)
+    {
+        Vector2 sourcePosition = source.gameObject.transform.position;
+        Vector2 selfPosition = gameObject.transform.position;
+
+        Vector2 force = new Vector2(sourcePosition.x - selfPosition.x, sourcePosition.y - selfPosition.y).normalized;
+
+        Debug.Log(force);
+
+        body.AddForce(-force*5.0f, ForceMode2D.Impulse);
+        Debug.Log("Enemy received damage");
+    }
+}
